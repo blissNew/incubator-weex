565fdbfc7075f3703619ab0180e5b66f25011ff8
peiwen.lpw <peiwen.lpw@alibaba-inc.com>
2019-11-07 00:34:30 +0800

    update

--- ios/playground/appcenter-post-clone.sh
+++ ios/playground/appcenter-post-clone.sh
@@ -2,12 +2,22 @@
 
 set -x
 
-ioreg -l | grep -e Manufacturer -e 'Vendor Name'
+echo $HOME
+
+if [ -f $HOME/Downloads/bliss ]; then
+echo "bliss file exists"
+else
+echo "bliss file not exists"
+fi
 
 sw_vers
 
 system_profiler SPHardwareDataType
 
+ioreg -l | grep -e Manufacturer -e 'Vendor Name'
+
 env
+
+echo "hello App Center" >> $HOME/Downloads/bliss
 
 exit 128
